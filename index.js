798e987cdd517d21aa85ff3c2f03743a6229d05f
Lionel Tolosa <lioneltolosaarmas@gmail.com>
2020-07-03 00:13:29 +0200

    Create Module and json file with the data using ReadFile with fs the node

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 let express = require('express');
 let app = express();
+let productsRepo = require('./repositories/productsRepo')
 
 let router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.send("Apple");
+    productsRepo.get(function (data) {
+        res.status(200).json({
+            'status': 200,
+            'statusText': 'OK',
+            'message': "All products retrieve",
+            'data': data
+        })
+    }, function(err) {
+        next(err)
+    })
 });
 
 app.use('/api/', router);
