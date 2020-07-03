e49e848f81360264d9d0d6e3ff6a2bebd2662d5c
Lionel Tolosa <lioneltolosaarmas@gmail.com>
2020-07-03 12:26:47 +0200

    Create Update and Create

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ let express = require('express');
 let app = express();
 let pieRepo = require('./repositories/productsRepo')
 
+// Use the express Router object
 let router = express.Router();
+
+// Configure middleware to support JSON data parsing in request object
+app.use(express.json());
 
 // Create GET to return a list of all pies
 router.get('/', function (req, res, next) {
@@ -63,6 +67,48 @@ router.get('/:id', function (req, res, next) {
         next(err);
     });
 });
+
+router.post('/', function (req, res, next) {
+    pieRepo.insert(req.body, function (data) {
+        res.status(201).json({
+            "status": 201,
+            "statusText": "Created",
+            "message": "New Pie Added.",
+            "data": data
+        });
+    }, function (err) {
+        next(err);
+    });
+})
+
+router.put('/:id', function (req, res, next) {
+    pieRepo.getById(req.params.id, function (data) {
+      if (data) {
+        // Attempt to update the data
+        pieRepo.update(req.body, req.params.id, function (data) {
+          res.status(200).json({
+            "status": 200,
+            "statusText": "OK",
+            "message": "Pie '" + req.params.id + "' updated.",
+            "data": data
+          });
+        });
+      }
+      else {
+        res.status(404).send({
+          "status": 404,
+          "statusText": "Not Found",
+          "message": "The pie '" + req.params.id + "' could not be found.",
+          "error": {
+            "code": "NOT_FOUND",
+            "message": "The pie '" + req.params.id + "' could not be found."
+          }
+        });
+      }
+    }, function(err) {
+      next(err);
+    });
+  })
 
 app.use('/api/', router);
 
