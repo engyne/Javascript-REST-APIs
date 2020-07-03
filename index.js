f41eb26127b11fa180e5fe69b638c569a84f193f
Lionel Tolosa <lioneltolosaarmas@gmail.com>
2020-07-03 11:26:47 +0200

    Create GetById and Search Function

--- index.js
+++ index.js
@@ -1,20 +1,67 @@
 let express = require('express');
 let app = express();
-let productsRepo = require('./repositories/productsRepo')
+let pieRepo = require('./repositories/productsRepo')
 
 let router = express.Router();
 
+// Create GET to return a list of all pies
 router.get('/', function (req, res, next) {
-    productsRepo.get(function (data) {
+    pieRepo.get(function (data) {
         res.status(200).json({
-            'status': 200,
-            'statusText': 'OK',
-            'message': "All products retrieve",
-            'data': data
-        })
-    }, function(err) {
-        next(err)
-    })
+            "status": 200,
+            "statusText": "OK",
+            "message": "All pies retrieved.",
+            "data": data
+        });
+    }, function (err) {
+        next(err);
+    });
+});
+
+// Create GET/search?id=n&name=str to search for pies by 'id' and/or 'name'
+router.get('/search', function (req, res, next) {
+    let searchObject = {
+        "id": req.query.id,
+        "name": req.query.name
+    };
+
+    pieRepo.search(searchObject, function (data) {
+        res.status(200).json({
+            "status": 200,
+            "statusText": "OK",
+            "message": "All pies retrieved.",
+            "data": data
+        });
+    }, function (err) {
+        next(err);
+    });
+});
+
+// Create GET/id to return a single pie
+router.get('/:id', function (req, res, next) {
+    pieRepo.getById(req.params.id, function (data) {
+        if (data) {
+            res.status(200).json({
+                "status": 200,
+                "statusText": "OK",
+                "message": "All pies retrieved.",
+                "data": data
+            });
+        }
+        else {
+            res.status(404).send({
+                "status": 404,
+                "statusText": "Not Found",
+                "message": "The pie '" + req.params.id + "' could not be found.",
+                "error": {
+                    "code": "NOT_FOUND",
+                    "message": "The pie '" + req.params.id + "' could not be found."
+                }
+            });
+        }
+    }, function (err) {
+        next(err);
+    });
 });
 
 app.use('/api/', router);
