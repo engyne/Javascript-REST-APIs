e6e52a8f32c60d94c8473d5c8838257477ca1c7d
Lionel Tolosa <lioneltolosaarmas@gmail.com>
2020-07-06 11:46:32 +0200

    Create HanldlerErrors and LogErrr

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ let express = require('express');
 let app = express();
 let pieRepo = require('./repositories/productsRepo')
 
+// Helper Errors
+let errorHelper = require('./helpers/errorHelpers');
+
 // Use the express Router object
 let router = express.Router();
 
@@ -83,34 +86,100 @@ router.post('/', function (req, res, next) {
 
 router.put('/:id', function (req, res, next) {
     pieRepo.getById(req.params.id, function (data) {
-      if (data) {
-        // Attempt to update the data
-        pieRepo.update(req.body, req.params.id, function (data) {
-          res.status(200).json({
-            "status": 200,
-            "statusText": "OK",
-            "message": "Pie '" + req.params.id + "' updated.",
-            "data": data
-          });
-        });
-      }
-      else {
-        res.status(404).send({
-          "status": 404,
-          "statusText": "Not Found",
-          "message": "The pie '" + req.params.id + "' could not be found.",
-          "error": {
-            "code": "NOT_FOUND",
-            "message": "The pie '" + req.params.id + "' could not be found."
-          }
-        });
-      }
-    }, function(err) {
-      next(err);
+        if (data) {
+            // Attempt to update the data
+            pieRepo.update(req.body, req.params.id, function (data) {
+                res.status(200).json({
+                    "status": 200,
+                    "statusText": "OK",
+                    "message": "Pie '" + req.params.id + "' updated.",
+                    "data": data
+                });
+            });
+        }
+        else {
+            res.status(404).send({
+                "status": 404,
+                "statusText": "Not Found",
+                "message": "The pie '" + req.params.id + "' could not be found.",
+                "error": {
+                    "code": "NOT_FOUND",
+                    "message": "The pie '" + req.params.id + "' could not be found."
+                }
+            });
+        }
+    }, function (err) {
+        next(err);
     });
-  })
+})
+
+router.delete('/:id', function (req, res, next) {
+    pieRepo.getById(req.params.id, function (data) {
+        if (data) {
+            // Attempt to delete the data
+            pieRepo.delete(req.params.id, function (data) {
+                res.status(200).json({
+                    "status": 200,
+                    "statusText": "OK",
+                    "message": "The pie '" + req.params.id + "' is deleted.",
+                    "data": "Pie '" + req.params.id + "' deleted."
+                });
+            });
+        }
+        else {
+            res.status(404).send({
+                "status": 404,
+                "statusText": "Not Found",
+                "message": "The pie '" + req.params.id + "' could not be found.",
+                "error": {
+                    "code": "NOT_FOUND",
+                    "message": "The pie '" + req.params.id + "' could not be found."
+                }
+            });
+        }
+    }, function (err) {
+        next(err);
+    });
+})
+
+router.patch('/:id', function (req, res, next) {
+    pieRepo.getById(req.params.id, function (data) {
+        if (data) {
+            // Attempt to update the data
+            pieRepo.update(req.body, req.params.id, function (data) {
+                res.status(200).json({
+                    "status": 200,
+                    "statusText": "OK",
+                    "message": "Pie '" + req.params.id + "' patched.",
+                    "data": data
+                });
+            });
+        }
+        else {
+            res.status(404).send({
+                "status": 404,
+                "statusText": "Not Found",
+                "message": "The pie '" + req.params.id + "' could not be found.",
+                "error": {
+                    "code": "NOT_FOUND",
+                    "message": "The pie '" + req.params.id + "' could not be found."
+                }
+            });
+        }
+    }, function (err) {
+        next(err);
+    });
+})
 
 app.use('/api/', router);
+
+
+// Configure exception logger
+app.use(errorHelper.logErrors);
+// Configure client error handler
+app.use(errorHelper.clientErrorHandler);
+// Configure catch-all exception middleware last
+app.use(errorHelper.errorHandler);
 
 var server = app.listen(5000, function () {
     console.log('Node server is running on http://localhost:5000..');
